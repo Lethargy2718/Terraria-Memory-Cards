87a96530e13cc0260129a157edcb31369b7c7323
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-06-12 19:15:04 +0300

    feat: implement main menu, add sprites

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Main from './Components/Main'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='screen'>
+      <Main />
+    </div>
   )
 }
 
